c126e049cf7ce47be4bf3ce2c5c5bd5d7ec25c79
NguyenDucHy2302 <nguyenduchy2302@gmail.com>
2026-03-25 20:18:03 +0700

    Add 2 new API Request
    Change status , Get request by UserID

---
+++ AutoFeed_Backend/AutoFeed_Backend/Models/Requests/Request/UpdateRequestStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoFeed_Backend.Models.Requests.Request
+{
+    public class UpdateRequestStatusRequest   
+    {
+        /// <summary>Allowed values: pending, approved, rejected</summary>
+        public string Status { get; set; }
+    }
+}
