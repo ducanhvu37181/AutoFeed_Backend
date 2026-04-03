d7f1f83da9a4de0149d05d5c9b85f62f01918802
NguyenDucHy2302 <nguyenduchy2302@gmail.com>
2026-04-03 20:33:13 +0700

    add Email files
    Email setting, Email Service

---
+++ AutoFeed_Backend/AutoFeed_Backend_DAO/Settings/EmailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoFeed_Backend_DAO.Settings;
+
+public class EmailSettings
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string SenderEmail { get; set; } = null!;
+    public string SenderName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
