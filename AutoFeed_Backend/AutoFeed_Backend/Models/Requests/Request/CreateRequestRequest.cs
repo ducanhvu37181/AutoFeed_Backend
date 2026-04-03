9ff4b1184f7e1edace1ef4ab39ff26584e3831e2
NguyenDucHy2302 <nguyenduchy2302@gmail.com>
2026-04-03 20:03:53 +0700

    Fix request of Request
    fix create request and update request of Request

--- AutoFeed_Backend/AutoFeed_Backend/Models/Requests/Request/CreateRequestRequest.cs
+++ AutoFeed_Backend/AutoFeed_Backend/Models/Requests/Request/CreateRequestRequest.cs
@@ -4,6 +4,7 @@
     {
         public int UserId { get; set; }
         public string Type { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
     }
 }
