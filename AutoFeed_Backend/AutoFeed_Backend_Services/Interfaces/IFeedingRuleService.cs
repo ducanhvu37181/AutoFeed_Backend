66a7195528b33900c6150574acbf4ff239ca37ff
NguyenTrungKien2610 <kienntse170416@fpt.edu.vn>
2026-04-11 12:23:02 +0700

    update

--- AutoFeed_Backend/AutoFeed_Backend_Services/Interfaces/IFeedingRuleService.cs
+++ AutoFeed_Backend/AutoFeed_Backend_Services/Interfaces/IFeedingRuleService.cs
@@ -13,5 +13,6 @@ namespace AutoFeed_Backend_Services.Interfaces
         Task<bool> DisableDetailAsync(int detailId);
         Task<bool> UpdateDetailAsync(int detailId, RuleDetailUpdateDto dto);
         Task<bool> DeleteRuleAsync(int id);
+        Task<bool> AddDetailAsync(RuleDetailCreateDto dto);
     }
 }
