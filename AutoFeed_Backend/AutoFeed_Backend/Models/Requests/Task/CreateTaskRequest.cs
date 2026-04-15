8abed6942f3c698ad58e72d97c1ab785a73771d8
Vũ Đức Anh <ducanhvu43@gmail.com>
2026-04-15 19:22:31 +0700

    fix create Task missing time

--- AutoFeed_Backend/AutoFeed_Backend/Models/Requests/Task/CreateTaskRequest.cs
+++ AutoFeed_Backend/AutoFeed_Backend/Models/Requests/Task/CreateTaskRequest.cs
@@ -4,6 +4,7 @@ public class CreateTaskRequest
 {
     public string Title { get; set; }
     public string Description { get; set; }
-    public TimeOnly StartTime { get; set; }
-    public TimeOnly EndTime { get; set; }
+    // Accept time as string to avoid JSON parsing issues in some clients; controller will parse to TimeOnly.
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
 }
